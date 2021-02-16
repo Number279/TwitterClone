6232a77a7cdffce489a62af167e698ed1f73d507
Number279 <sawyercarroll@gmail.com>
2021-02-16 08:44:03 -0600

    first commit

---
+++ src/main/java/com/tts/switter/SwitterApplication.java
@@ -0,0 +1,13 @@
+package com.tts.switter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/switter/SwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.switter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
